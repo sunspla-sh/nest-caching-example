4e87428dd6fa5821c52ddaaff7a7c353a22e9d6c
jack <jrfisch95@gmail.com>
2023-08-31 19:10:01 -0400

    add breakpoint test of GET /cats route

--- k6/cat-test.js
+++ k6/cat-test.js
@@ -1,21 +1,7 @@
-//import necessary module
-
 import http from 'k6/http';
 
 export default function () {
   const url = 'http://localhost:3000/cats';
-  const catPayload = JSON.stringify({
-    name: 'jimbo',
-    age: 2,
-  });
 
-  const params = {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  };
-
-  const res = http.post(url, payload, params);
-
-  console.log(res.body);
+  http.get(url);
 }
---
+++ k6/cats/config.js
@@ -0,0 +1,11 @@
+export const thresholdSettings = {
+  http_req_failed: [{ threshold: 'rate<0.01', abortOnFail: true }],
+  http_req_duration: ['p(99)<1000'],
+};
+
+export const breakpointWorkload = {
+  executor: 'ramping-arrival-rate', //assure load increase if the system slows
+  preAllocatedVUs: 10,
+  maxVUs: 100000,
+  stages: [{ duration: '1m', target: 100000 }],
+};
